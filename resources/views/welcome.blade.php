5a76d4e5bbcdff33e0237f4e162dd992d62f41ec
Technest-India <support@technestindia.in>
2025-04-24 21:35:14 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,11 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" ng-app="myApp">
     <head>
         @yield("meta")
+        
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
     <body ng-controller="homeController as home">
+        <h1>TECH NEST INDIA</h1>
         @yield('content')
         @yield('script')
     </body>
