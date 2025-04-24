ba1e078a51bd81342cbfe7c3e0434513546522d2
Technest-India <support@technestindia.in>
2025-04-24 21:16:22 +0530

    changes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
-
+// Main Route file
 /*
 |--------------------------------------------------------------------------
 | Web Routes
